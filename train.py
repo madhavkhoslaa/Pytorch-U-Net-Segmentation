fff9b1dbe8f4ab7951aa448c8ac431ae620d57a3
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-25 15:48:31 +0530

    train

--- train.py
+++ train.py
@@ -4,14 +4,18 @@ from model.Unet import UNeT
 from loss import diceloss
 from torch.utils.data import DataLoader
 from dataloader.dataloader import ImageLoader, TrainSet, TestSet
+from hyperparams import hyperparameters
+params= hyperparameters(train_percentage= 0.6, batch_size= 4)
 net= UNeT(n_class=1)
 DATASET="/path/to/dataset"
 
-Images= ImageLoader(Images="/Path/to/images", Annotations="/Path/to/Annotations", train_percentage= 0.6)
+Images= ImageLoader(Images="/Path/to/images", Annotations="/Path/to/Annotations", train_percentage= params['train_percentage'])
 Train= TrainSet(Images.train_set, extension="JPEG", transform=None)
 Test= TestSet(Images.test_set, extension="JPEG", transform=None)
 
-Train_Loader= DataLoader(Train, batch_size=4, shuffle=True)
-Test_Loadrt= DataLoader(Test, batch_size=4, shuffle=True)
+Train_Loader= DataLoader(Train, batch_size=params['batch_size'], shuffle=True)
+Test_Loadrt= DataLoader(Test, batch_size=params['batch_size'], shuffle=True)
 
 #TODO: Train Loop
+for i, data in enumerate(Train_Loader):
+    pass
