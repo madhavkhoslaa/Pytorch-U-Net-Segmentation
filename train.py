4474a5af2b50ec911d0a36bc2120f51b5cc99859
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-25 15:27:54 +0530

    Training

--- train.py
+++ train.py
@@ -8,6 +8,10 @@ net= UNeT(n_class=1)
 DATASET="/path/to/dataset"
 
 Images= ImageLoader(Images="/Path/to/images", Annotations="/Path/to/Annotations", train_percentage= 0.6)
-trainloader= TrainSet
+Train= TrainSet(Images.train_set, extension="JPEG", transform=None)
+Test= TestSet(Images.test_set, extension="JPEG", transform=None)
 
+Train_Loader= DataLoader(Train, batch_size=4, shuffle=True)
+Test_Loadrt= DataLoader(Test, batch_size=4, shuffle=True)
 
+#TODO: Train Loop
