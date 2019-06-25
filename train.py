085b5c5bda4679fae12cffcb29ae7987c2a4f50a
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-25 14:22:02 +0530

    Traninng code in process

--- train.py
+++ train.py
@@ -1,3 +1,13 @@
 import torch.nn as nn
 import torch.optim as optim
+from model.Unet import UNeT
+from loss import diceloss
+from torch.utils.data import DataLoader
+from dataloader.dataloader import ImageLoader, TrainSet, TestSet
+net= UNeT(n_class=1)
+DATASET="/path/to/dataset"
+
+Images= ImageLoader(Images="/Path/to/images", Annotations="/Path/to/Annotations", train_percentage= 0.6)
+trainloader= TrainSet
+
 
