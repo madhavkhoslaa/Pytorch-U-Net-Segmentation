a7b371ca2803f97bcc2aafd0cdc9874bd872b4ea
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-26 18:30:16 +0530

    -

--- train.py
+++ train.py
@@ -9,12 +9,12 @@ params= hyperparameters(train_percentage= 0.6, batch_size= 4)
 net= UNeT(n_class=1)
 DATASET="/path/to/dataset"
 
-Images= ImageLoader(Images="/Path/to/images", Annotations="/Path/to/Annotations", train_percentage= params['train_percentage'])
+Images= ImageLoader(Images="/Path/to/images", Annotations="/Path/to/Annotations", train_percentage= params.hyperparameters['train_percentage'])
 Train= TrainSet(Images.train_set, extension="JPEG", transform=None)
 Test= TestSet(Images.test_set, extension="JPEG", transform=None)
 
-Train_Loader= DataLoader(Train, batch_size=params['batch_size'], shuffle=True)
-Test_Loadrt= DataLoader(Test, batch_size=params['batch_size'], shuffle=True)
+Train_Loader= DataLoader(Train, batch_size=params.hyperparameters['batch_size'], shuffle=True)
+Test_Loadrt= DataLoader(Test, batch_size=params.hyperparameters['batch_size'], shuffle=True)
 
 #TODO: Train Loop
 for i, data in enumerate(Train_Loader):
