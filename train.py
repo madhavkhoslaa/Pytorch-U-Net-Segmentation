adc979e221d2a5f85d89556d4640deb263bdc91c
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-26 18:32:17 +0530

    train and ann dir

--- train.py
+++ train.py
@@ -3,19 +3,19 @@ import torch.optim as optim
 from model.Unet import UNeT
 from loss import diceloss
 from torch.utils.data import DataLoader
-from dataloader.dataloader import ImageLoader, TrainSet, TestSet
 from hyperparams.hyperparams import hyperparameters
-params= hyperparameters(train_percentage= 0.6, batch_size= 4)
-net= UNeT(n_class=1)
-DATASET="/path/to/dataset"
+from dataloader.dataloader import ImageLoader, TrainSet, TestSet
+params = hyperparameters(train_percentage=0.6, batch_size=4)
+net = UNeT(n_class=1)
+IMAGE_DIR = "/home/fatjuicyboi/DataSets/AerialImageDataset/train/images/"
+ANNOTATIONS_DIR = "/home/fatjuicyboi/DataSets/AerialImageDataset/train/gt/"
+Images = ImageLoader(
+    Images=IMAGE_DIR,
+    Annotations=ANNOTATIONS_DIR,
+    train_percentage=params.hyperparameters['train_percentage'],
+    extension="tif")
+Train = TrainSet(Images.train_set, extension="tif", transform=None)
+Test = TestSet(Images.test_set, extension="tif", transform=None)
 
-Images= ImageLoader(Images="/Path/to/images", Annotations="/Path/to/Annotations", train_percentage= params.hyperparameters['train_percentage'])
-Train= TrainSet(Images.train_set, extension="JPEG", transform=None)
-Test= TestSet(Images.test_set, extension="JPEG", transform=None)
-
-Train_Loader= DataLoader(Train, batch_size=params.hyperparameters['batch_size'], shuffle=True)
-Test_Loadrt= DataLoader(Test, batch_size=params.hyperparameters['batch_size'], shuffle=True)
-
-#TODO: Train Loop
-for i, data in enumerate(Train_Loader):
-    pass
+for i in range(len(Train)):
+    print(Train[i])
