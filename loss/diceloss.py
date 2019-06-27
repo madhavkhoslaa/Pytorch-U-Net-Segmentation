244c51b5bafb03c8e1a0dd8287c5a15b9a7a075b
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-27 11:45:26 +0530

    Calculate loss

--- loss/diceloss.py
+++ loss/diceloss.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import torch.functional as F
 
 def dice_loss(pred, target, smooth=1.):
     pred = pred.contiguous()
@@ -9,3 +9,13 @@ def dice_loss(pred, target, smooth=1.):
     loss = (1 - ((2. * intersection + smooth) /
                  (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
     return loss.mean()
+
+def calc_loss(pred, target, metrics, bce_weight=0.5):
+    bce = torch.binary_cross_entropy_with_logits(pred, target)
+    pred = torch.sigmoid(pred)
+    dice = dice_loss(pred, target)
+    loss = bce * bce_weight + dice * (1 - bce_weight)
+    metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
+    metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
+    metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
+    return loss
