67ce94b4f5e06373e3bb39ef12dafa0efc1ca083
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-06-27 17:06:37 +0530

    DataLoader test

--- test/test_iter.py
+++ test/test_iter.py
@@ -7,15 +7,14 @@ from hyperparams.hyperparams import hyperparameters
 from dataloader.dataloader import ImageLoader, TrainSet, TestSet
 params = hyperparameters(train_percentage=0.6, batch_size=4)
 net = UNeT(n_class=1)
-IMAGE_DIR = "/home/fatjuicyboi/DataSets/AerialImageDataset/train/images/"
-ANNOTATIONS_DIR = "/home/fatjuicyboi/DataSets/AerialImageDataset/train/gt/"
+IMAGE_DIR = "/Users/madhav/DataSets/AerialImageDataset/train/images/*.tif"
+ANNOTATIONS_DIR = "/Users/madhav/DataSets/AerialImageDataset/train/gt/*.tif"
 Images = ImageLoader(
     Images=IMAGE_DIR,
     Annotations=ANNOTATIONS_DIR,
-    train_percentage=params.hyperparameters['train_percentage'],
-    extension="tif")
+    train_percentage=params.hyperparameters['train_percentage'])
 Train = TrainSet(Images.train_set, extension="tif", transform=None)
 Test = TestSet(Images.test_set, extension="tif", transform=None)
-
+print(Images.test_set)
 for i in range(len(Train)):
     print(Train[i])
