119cc52fdb7a61dece0a2027ff8fbcc77effa285
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-07-01 14:08:44 +0530

    config parser

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import json
 
-class config():
+class Config():
     def __init__(self, config_file):
         with open(config_file) as conf_file:
             self.conf= json.load(conf_file)
-    def get_config(self):
+    def load_conf(self):
         return self.conf
