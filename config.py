3d933ea818720d0237c0de2c5805079771c047c8
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-07-02 12:26:31 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import json
-
-class Config():
-    def __init__(self, config_file):
-        with open(config_file) as conf_file:
-            self.conf= json.load(conf_file)
-    def load_conf(self):
-        return self.conf
