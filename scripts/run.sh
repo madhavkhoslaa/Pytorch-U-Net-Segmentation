e13f46e8311b3b7bdaf92362beb28e97bdbe1f19
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-07-10 12:24:24 +0530

    Run Script

---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+#downloads data, libraries and trains the model
+cd scripts
+chmod +x get-requirments.sh
+chmod +x getdataset.sh
+chmod +x train.sh
+./get-requirments.sh
+./getdataset.sh
+./train.sh
