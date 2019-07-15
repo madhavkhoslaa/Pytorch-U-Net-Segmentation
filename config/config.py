748d14febf45992decaf30438b07a0326f87c62a
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-07-15 14:11:33 +0530

    comments

--- config/config.py
+++ config/config.py
@@ -1,6 +1,8 @@
 import json
 
 class Config():
+    """Creates an object which takes a json file and returns a 
+        dictionary when load_conf is called on the object"""
     def __init__(self, config_file):
         with open(config_file) as conf_file:
             self.conf= json.load(conf_file)
