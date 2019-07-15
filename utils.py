3279d18b53fe29b78a39499e7a1ac76fc4ecca97
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-07-15 13:49:19 +0530

    to calculate output size of the model

---
+++ utils.py
@@ -0,0 +1,49 @@
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from dataloader.dataloader import ImageLoader, ImageList
+import torch
+import torchvision
+from torchvision import transforms
+from config.config import Config
+
+
+conf= Config('./config.json')
+conf= conf.load_conf()
+IMAGE_DIR= conf["Train Data"]
+ANNOTATIONS_DIR= conf["Annotations Data"]
+TEST_DATA= conf["Test Data"]
+MODEL_SAVE= conf["Model Save"]
+IMAGE_RESOLUTION= tuple(map(int, conf['Resolution'].split(',')))
+transforms_compose = transforms.Compose([transforms.Resize(IMAGE_RESOLUTION[:2]),  transforms.ToTensor()])
+
+Images = ImageList(
+    Images=IMAGE_DIR,
+    Annotations=ANNOTATIONS_DIR,
+    train_percentage=0.1,
+    extension="tif")
+Train = ImageLoader(
+    data= Images.train_set,
+    extension="tif",
+    transform=transforms_compose)
+Test = ImageLoader(data= Images.test_set, extension="tif", transform=transforms_compose)
+TrainLoder = DataLoader(
+    Train,
+    batch_size=1,
+    shuffle=True)
+def get_out_size(model):
+    for epoch in range(1):
+        for i, data in enumerate(TrainLoder, 0):
+            if torch.cuda.is_available():
+                inputs, labels = data["Image"].cuda(), data["Label"].cuda()
+            else:
+                inputs, labels = data["Image"], data["Label"]
+            if torch.cuda.is_available():
+                outputs = model(
+                    inputs.type(
+                        torch.cuda.FloatTensor))
+            else:
+                outputs = model(
+                    inputs.type(
+                        torch.FloatTensor))
+            return outputs.size()
+
