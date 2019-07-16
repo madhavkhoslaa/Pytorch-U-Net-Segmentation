f2263e4449c8cc5ae6a1c7042e3431faaa663ca4
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-07-16 17:37:43 +0530

    pep8

--- train.py
+++ train.py
@@ -10,17 +10,18 @@ import torch
 from torchvision import transforms
 from config.config import Config
 from utils.utils import get_out_size
-from tqdm import tqdm 
+from tqdm import tqdm
 
 
-conf= Config('./config.json')
-conf= conf.load_conf()
-IMAGE_DIR= conf["Train Data"]
-ANNOTATIONS_DIR= conf["Annotations Data"]
-TEST_DATA= conf["Test Data"]
-MODEL_SAVE= conf["Model Save"]
-IMAGE_RESOLUTION= tuple(map(int, conf['Resolution'].split(',')))
-transforms_compose = transforms.Compose([transforms.Resize(IMAGE_RESOLUTION[:2]),  transforms.ToTensor()])
+conf = Config('./config.json')
+conf = conf.load_conf()
+IMAGE_DIR = conf["Train Data"]
+ANNOTATIONS_DIR = conf["Annotations Data"]
+TEST_DATA = conf["Test Data"]
+MODEL_SAVE = conf["Model Save"]
+IMAGE_RESOLUTION = tuple(map(int, conf['Resolution'].split(',')))
+transforms_compose = transforms.Compose(
+    [transforms.Resize(IMAGE_RESOLUTION[:2]), transforms.ToTensor()])
 
 params = hyperparameters(
     train_percentage=0.6,
@@ -28,9 +29,9 @@ params = hyperparameters(
     epoch=4,
     n_classes=1)
 if torch.cuda.is_available():
-    net = UNeT(n_classes= 1, n_channels=3).cuda()
+    net = UNeT(n_classes=1, n_channels=3).cuda()
 else:
-    net = UNeT(n_classes= 1, n_channels=3)
+    net = UNeT(n_classes=1, n_channels=3)
 
 
 Images = ImageList(
@@ -40,10 +41,13 @@ Images = ImageList(
     extension="tif")
 loss_val = Loss()
 Train = ImageLoader(
-    data= Images.train_set,
+    data=Images.train_set,
     extension="tif",
     transform=transforms_compose)
-Test = ImageLoader(data= Images.test_set, extension="tif", transform=transforms_compose)
+Test = ImageLoader(
+    data=Images.test_set,
+    extension="tif",
+    transform=transforms_compose)
 TrainLoder = DataLoader(
     Train,
     batch_size=params.hyperparameters["batch_size"],
@@ -54,9 +58,10 @@ ValLoader = DataLoader(
     shuffle=True)
 criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-MODEL_OUTPUT_SIZE= get_out_size(net)
+MODEL_OUTPUT_SIZE = get_out_size(net)
 print("MODEL_OUTPUT_SIZE", MODEL_OUTPUT_SIZE)
-for epoch in tqdm(range(params.hyperparameters["epoch"]), desc= "Training Loop"):
+for epoch in tqdm(
+        range(params.hyperparameters["epoch"]), desc="Training Loop"):
     metrics = defaultdict()
     running_loss = 0.0
     for i, data in enumerate(TrainLoder, 0):
@@ -72,12 +77,23 @@ for epoch in tqdm(range(params.hyperparameters["epoch"]), desc= "Training Loop")
             outputs = net(
                 inputs.type(
                     torch.FloatTensor))
-        loss = criterion(input= outputs.view(-1,1,1,1)[:], target=labels.view(-1,1,1,1).type(torch.FloatTensor)[:372000])
+        loss = criterion(input=outputs.view(-1,
+                                            1,
+                                            1,
+                                            1)[:],
+                         target=labels.view(-1,
+                                            1,
+                                            1,
+                                            1).type(torch.FloatTensor)[:372000])
         loss.backward()
         optimizer.step()
         print(outputs.size())
         running_loss += loss.item()
-        print("Epoch: {} | Loss: {} | Instance: {}".format(int(epoch),loss.item(), i))
+        print(
+            "Epoch: {} | Loss: {} | Instance: {}".format(
+                int(epoch),
+                loss.item(),
+                i))
         print("Running loss|", running_loss)
-torch.save(net.state_dict() , MODEL_SAVE+ "/model.pt")
+torch.save(net.state_dict(), MODEL_SAVE + "/model.pt")
 print("Model saved")
