cff74caa87f32c0de239aaf99cf0ce9fd896642b
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-07-16 16:22:32 +0530

    Added comments to gen_colors

--- utils/one_hot_encoder.py
+++ utils/one_hot_encoder.py
@@ -1,18 +1,30 @@
 import skimage
 import glob
+from tqdm import tqdm
+
 
 class HotEncoder():
-    def __init__(self, dir, extenstion):
+    def __init__(self, dir, extension, is_binary= True):
         self.dir= dir
-        self.extension= extenstion
-        self.color= set()
+        self.extension= extension
+        self.is_binary= is_binary
+        if is_binary:
+            self.color= set(((255, 255, 255), (0, 0, 0)))
+        else:
+            self.color= set()
+
     def gen_colors(self):
-        images= glob.glob(dir+'/*'+ self.extension)
-        for img in images:
+        """Iterates through the entire dataset and finds the total colours
+            in the images so that they can be used to one hot the image matrix 
+            for the training data"""
+        if self.is_binary:
+            return self.color
+        images= glob.glob(self.dir+'/*.'+ self.extension)
+        for img in tqdm(images, desc="Generating Color Pallte to Hot Encode"):
             image= skimage.io.imread(img)
-            h, w= tuple(image.shape())
-            for x in range(w):
-                for y in range(h):
+            shape_= image.shape
+            for x in range(shape_[0]):
+                for y in range(shape_[1]):
                     self.color.add(tuple(image[x][y][:]))
         return self.color
 
