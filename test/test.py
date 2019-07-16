f730f1befe3f1a761cb3a13d1f397a996daea1e7
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-07-16 16:24:05 +0530

    testing the modules

---
+++ test/test.py
@@ -0,0 +1,23 @@
+from utils.one_hot_encoder import HotEncoder
+import  numpy as np
+import skimage
+encoder= HotEncoder(dir= "/Users/madhav/Desktop/", extension="jpeg", is_binary= False)
+color_dict= encoder.gen_colors()
+print(color_dict)
+print(len(color_dict))
+
+def rgb_to_onehot(rgb_arr, color_dict):
+    num_classes = len(color_dict)
+    shape = rgb_arr.shape[:2]+(num_classes,)
+    arr = np.zeros( shape, dtype=np.int8 )
+    for i, cls in enumerate(color_dict):
+        arr[:,:,i] = np.all(rgb_arr.reshape( (-1,3) ) == color_dict[i], axis=1).reshape(shape[:2])
+    return arr
+
+
+def onehot_to_rgb(onehot, color_dict):
+    single_layer = np.argmax(onehot, axis=-1)
+    output = np.zeros( onehot.shape[:2]+(3,) )
+    for k in color_dict.keys():
+        output[single_layer==k] = color_dict[k]
+    return np.uint8(output)
