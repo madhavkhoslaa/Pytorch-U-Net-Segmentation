b819a035ab0112f2e0e34a7b11ef2a31f07f0fdd
Madhav Khosla <madhav.khoslaa@gmail.co>
2019-07-19 14:46:59 +0530

    Fixed bug where even different colors were given same class

--- utils/one_hot_encoder.py
+++ utils/one_hot_encoder.py
@@ -1,6 +1,7 @@
 import skimage
 import glob
 from tqdm import tqdm
+import numpy as np
 
 
 class HotEncoder():
@@ -9,7 +10,7 @@ class HotEncoder():
         self.extension = extension
         self.is_binary = is_binary
         if is_binary:
-            self.color = set(((255, 255, 255), (0, 0, 0)))
+            self.color = {(0, 0, 0): 1, (255, 255, 255): 2}
         else:
             self.color = dict()
 
@@ -19,20 +20,35 @@ class HotEncoder():
             for the training data"""
         if self.is_binary:
             return self.color
-        images = glob.glob(self.dir + '/*.' + self.extension)
-        for img in tqdm(images, desc="Generating Color Pallte to Hot Encode"):
-            image = skimage.io.imread(img)
-            shape_ = image.shape
+        else:
             n_color=1
-            for x in range(shape_[0]):
-                for y in range(shape_[1]):
-                    clr= tuple(image[x][y][:])
-                    if clr not in self.color.keys():
-                        self.color.update({clr: n_color})
-                        n_color+=1
-                    else:
-                        pass
+            images = glob.glob(self.dir + '/*.' + self.extension)
+            for img in tqdm(images, desc="Generating Color Pallte to Hot Encode"):
+                image = skimage.io.imread(img)
+                shape_ = image.shape
+                for x in range(shape_[0]):
+                    for y in range(shape_[1]):
+                        clr= tuple(image[x][y][:])
+                        if clr not in self.color.keys():
+                            self.color.update({clr: n_color})
+                            n_color+=1
+                        else:
+                            pass
         return self.color
 
-    def gen_class_image(self):
-        pass
+    def PerPixelClassMatrix(self, Image):
+        """Takes an Image and returns a per pixel class
+            identification map"""
+        class_list= []
+        shape_= Image.shape
+        for x in range(shape_[0]):
+            for y in range(shape_[1]):
+                clr= tuple(Image[x][y][:])
+                if clr in self.color.keys():
+                    class_list.append(self.color[clr])
+                else:
+                    pass
+        return class_list
+
+
+        
