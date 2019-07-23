4463bb11fa364c9f1317ee918627b947a5e97c9c
Madhav Khosla <madhav.khoslaa@gmail.co>
2019-07-23 14:27:54 +0530

    Tests

--- test/test.py
+++ test/test.py
@@ -1,9 +1,11 @@
+import os
+os.chdir("..")
 from utils.one_hot_encoder import HotEncoder
 import  numpy as np
 import torch
 import skimage
 from torch.nn.functional import one_hot
-img__= skimage.io.imread("/Users/madhav/DataSets/data_semantics/training/semantic_rgb/000035_10.tif")
+img__= skimage.io.imread("/Users/madhav/DataSets/data_semantics/training/semantic_rgb/000034_10.tif")
 encoder= HotEncoder(dir= '/Users/madhav/DataSets/data_semantics/training/semantic_rgb', extension="tif", is_binary= False, n_classes= 30)
 color_dict= encoder.gen_colors()
 onehottt= encoder.PerPixelClassMatrix(img__)
