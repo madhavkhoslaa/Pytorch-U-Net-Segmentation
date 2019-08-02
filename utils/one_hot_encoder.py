14c7f6c44d2dbd948ab3ca7c5578882d35535412
Madhav Khosla <madhav.khoslaa@gmail.co>
2019-08-02 13:16:54 +0530

    Some random fixes

--- utils/one_hot_encoder.py
+++ utils/one_hot_encoder.py
@@ -5,16 +5,15 @@ import numpy as np
 
 
 class HotEncoder():
-    def __init__(self, n_classes,dir, extension, is_binary=False):
+    def __init__(self, dir, extension, is_binary= False):
         self.dir = dir
         self.extension = extension
-        self.is_binary = is_binary
-        self.n_classes= n_classes
-        if is_binary:
+        self.is_binary= is_binary
+        if self.is_binary:
             self.color = {(0, 0, 0): 0, (255, 255, 255): 1}
         else:
             self.color = dict()
-    
+
     def gen_colors(self):
         """Iterates through the entire dataset and finds the total colours
             in the images so that they can be used to one hot the image matrix
@@ -22,47 +21,60 @@ class HotEncoder():
         if self.is_binary:
             return self.color
         else:
-            n_color=1
+            n_color = 1
             images = glob.glob(self.dir + '/*.' + self.extension)
-            for img in tqdm(images, desc="Generating Color Pallte to Hot Encode"):
-                if self.extension=='tif':
-                    image= skimage.external.tifffile.imread(img)
+            for img in tqdm(
+                    images, desc="Generating Color Pallte to Hot Encode"):
+                if self.extension == 'tif':
+                    image = skimage.external.tifffile.imread(img)
                 else:
                     image = skimage.io.imread(img)
                 shape_ = image.shape
                 for x in range(shape_[0]):
                     for y in range(shape_[1]):
-                        clr= tuple(image[x][y][:])
+                        clr = tuple(image[x][y][:])
                         if clr not in self.color.keys():
                             self.color.update({clr: n_color})
-                            n_color+=1
+                            n_color += 1
                         else:
                             pass
         return self.color
 
-    def PerPixelClassMatrix(self, Image):
+    def PerPixelClassMatrix(self, Image, color_dict=None):
         """Takes an Image and returns a per pixel class
             identification map"""
-        shape_= Image.shape
-        class_map= np.zeros(shape= (shape_[0], shape_[1]), dtype=float)
+        if color_dict is None:
+            color_dict = self.color_dict
+        else:
+            assert isinstance(color_dict, dict)
+        shape_ = Image.shape
+        class_map = np.zeros(shape=(shape_[0], shape_[1]), dtype=float)
         for x in range(shape_[0]):
             for y in range(shape_[1]):
-                clr= tuple(Image[x][y][:])[:3]
-                class_map[x][y]= self.color[clr]
+                clr = tuple(Image[x][y][:])[:3]
+                class_map[x][y] = color_dict[clr]
         return class_map
+
     def hotEncode(self, class_map):
         """Inputs the class map of the image and returns the hot encoded
         N channel output. Each channel represents the presence of a class per pixel"""
-        assert isinstance(class_map, np.ndarray), "Class map has to be an ndarray and not {}".format(type(class_map))
-        shape_= class_map.shape
-        encoded= np.zeros(shape= (shape_[0], shape_[1], self.n_classes), dtype=float)
+        assert isinstance(
+            class_map, np.ndarray), "Class map has to be an ndarray and not {}".format(
+            type(class_map))
+        shape_ = class_map.shape
+        encoded = np.zeros(
+            shape=(
+                shape_[0],
+                shape_[1],
+                self.n_classes),
+            dtype=float)
         for x in range(shape_[0]):
             for y in range(shape_[1]):
-                category= int(class_map[x][y])
-                encoded[x][y][category]= 1
+                category = int(class_map[x][y])
+                encoded[x][y][category] = 1
         return encoded
 
-    def channelSplit(self,Nch_Hot_encoded):
+    def channelSplit(self, Nch_Hot_encoded):
         """The hot encoded Image is stored as stack of Hot encoded ndarrays
             this function returns a list of cll the channels"""
-        return np.dsplit(Nch_Hot_encoded,Nch_Hot_encoded.shape[-1])
+        return np.dsplit(Nch_Hot_encoded, Nch_Hot_encoded.shape[-1])
