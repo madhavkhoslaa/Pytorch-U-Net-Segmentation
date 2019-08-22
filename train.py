14a59a175e10c6f25c54066d2418716ec5377cbe
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-08-22 20:30:29 +0530

    /

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ TEST_DATA = conf["Test Data"]
 MODEL_SAVE = conf["Model Save"]
 IMAGE_RESOLUTION = tuple(map(int, conf['Resolution'].split(',')))
 transforms_compose = transforms.Compose(
-    [])
+    [transforms.ToTensor()])
 
 params = hyperparameters(
     train_percentage=0.6,
@@ -32,24 +32,23 @@ if torch.cuda.is_available():
     net = UNeT(n_classes=29, n_channels=3).cuda()
 else:
     net = UNeT(n_classes=29, n_channels=3)
-encoder= HotEncoder(is_binary= False, dir= ANNOTATIONS_DIR, extension="tif")
+encoder= HotEncoder(is_binary= False, dir= ANNOTATIONS_DIR, extension="png")
 color_dict= encoder.gen_colors()
-
 Images = ImageList(
     Images=IMAGE_DIR,
     Annotations=ANNOTATIONS_DIR,
     train_percentage=0.9,
-    extension="tif")
+    extension="png")
 loss_val = Loss()
 Train = ImageLoader(
     encoder_obj= encoder,
     data=Images.train_set,
-    extension="tif",
+    extension="png",
     transform=transforms_compose)
 Test = ImageLoader(
     encoder_obj= encoder,
     data=Images.test_set,
-    extension="tif",
+    extension="tifngng",
     transform=transforms_compose)
 TrainLoader = DataLoader(
     Train,
@@ -64,26 +63,20 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 
 for i, data in enumerate(TrainLoader, 0):
-    inputs, labels= data["Image"], data["Label"]
+    inputs, labels= data["Image"].permute(0, 3, 1, 2), data["Label"].permute(0, 3, 1, 2)
+    print(inputs, labels)
+
 for epoch in tqdm(
         range(params.hyperparameters["epoch"]), desc="Training Loop"):
     metrics = defaultdict()
     running_loss = 0.0
     for i, data in enumerate(TrainLoader, 0):
         if torch.cuda.is_available():
-            inputs, labels = data["Image"].permute(0, 3, 1, 2).cuda(), data["Label"].permute(0, 3, 1, 2).cuda()
+            inputs, labels = data["Image"].cuda(), data["Label"].cuda()
         else:
-            inputs, labels = data["Image"].permute(0, 3, 1, 2), data["Label"].permute(0, 3, 1, 2)
-        if torch.cuda.is_available():
-            outputs = net(
-                inputs.type(
-                    torch.cuda.FloatTensor))
-        else:
-            outputs = net(
-                inputs.type(
-                    torch.FloatTensor))
-        loss = criterion(input=outputs,
-                         target=labels)
+            inputs, labels = data["Image"], data["Label"]
+        outputs = net(inputs)
+        loss = criterion(input=outputs,target=labels)
         #There was a dimension strip above here check if you need it again, lad.
         loss.backward()
         optimizer.step()
@@ -95,4 +88,4 @@ for epoch in tqdm(
                 i))
         print("Running loss|", running_loss)
 torch.save(net.state_dict(), MODEL_SAVE + "/model.pt")
-print("Model saved")     
+print("Model saved")
