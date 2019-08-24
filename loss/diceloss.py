66feee72c43f5248f49a1fde47009b9da3c43271
Madhav Khosla <madhav.khoslaa@gmail.com>
2019-08-24 21:46:44 +0530

    Better diceloss function

--- loss/diceloss.py
+++ loss/diceloss.py
@@ -5,20 +5,9 @@ import torch.functional as F
 
 class Loss:
     @classmethod
-    def dice_loss(self, pred, target, smooth=1.):
-        pred = pred.contiguous()
-        target = target.contiguous()
-        intersection = (pred * target).sum(dim=2).sum(dim=2)
-        loss = (1 - ((2. * intersection + smooth) /
-                     (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
-        return loss.mean()
-
-    def calc_loss(self, pred, target, metrics, bce_weight=0.5):
-        bce = torch.nn.functional.binary_cross_entropy(pred, target)
-        pred = torch.sigmoid(pred)
-        dice = self.dice_loss(pred, target)
-        loss = bce * bce_weight + dice * (1 - bce_weight)
-        metrics['bce'] += bce.data.cpu().numpy() * target.size(0)
-        metrics['dice'] += dice.data.cpu().numpy() * target.size(0)
-        metrics['loss'] += loss.data.cpu().numpy() * target.size(0)
-        return loss
+    def dice_loss(input, target):
+        smooth = 1.
+        iflat = input.view(-1)
+        tflat = target.view(-1)
+        intersection = (iflat * tflat).sum()
+        return 1 - ((2. * intersection + smooth)/(iflat.sum() + tflat.sum() + smooth))
